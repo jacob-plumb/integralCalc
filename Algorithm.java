00de18a4712b7c717f4614b8755248ef995eebe8
Jacob Plumb <jacobplumb01@gmail.com>
2018-01-18 14:31:08 -0800

    Parsed algorithm into terms
    Issue with determining pos/neg

--- Algorithm.java
+++ Algorithm.java
@@ -4,22 +4,22 @@ public class Algorithm
     private int A;
     private int B;
     private String[] terms;
-    
+
     public Algorithm(String equation)
     {
         equation.replaceAll("\\s", "");
         String tempString = equation;
         int termCount = (equation.length() - equation.replace("+", "").length()) + (equation.length() -equation.replace("-", "").length());
-        terms = new String[termCount++];
+        terms = new String[termCount + 1];
         for(int i = 0; i <= termCount; i++)
         {
-            String symbol = posOrNeg(tempString);
+            String symbol = posOrNeg(tempString); // ISSUE IS RIGHT HERE
             int index = tempString.indexOf(symbol);
             terms[i] = tempString.substring(0, index); //DOESNT INCLUDE SYMBOL
-            tempString = tempString.substring(index+2);
+            System.out.println("Successful iteration " + i);
         }
     }
-    
+
     public String posOrNeg(String function)
     {
         String symbol;
@@ -35,7 +35,7 @@ public class Algorithm
         }
         return symbol;
     }
-    
+
     public void printTerms()
     {
         for(int i = 0; i < terms.length; i++)
